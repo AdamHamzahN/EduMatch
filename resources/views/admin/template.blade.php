805a35b1405981ee629add51ddf26e5bbedf6fac
Adam Hamzah Nurwahyudi <adamhamzahofficial13@gmail.com>
2025-05-31 17:51:55 +0700

    update

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -2,25 +2,3 @@
 @section('page-name')
     @yield('page_name')
 @endsection
-
-@section('body')
-
-    <body>
-        <div class="container-fluid">
-            <div class="row" style="height: 100vh;">
-                <div class="col-1 bg-primary text-white d-flex flex-column p-1">
-                    <nav class="nav flex-column">
-                        <a class="nav-link text-white" href="/admin/chat">Chat</a>
-                    </nav>
-                </div>
-
-                <div class="col-5 border p-3">
-                    @yield('mid-content')
-                </div>
-
-                <div class="col-6 border p-1">
-                    @yield('right-content')
-                </div>
-            </div>
-        </div>
-    @endsection
